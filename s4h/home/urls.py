e9a29087d038f895c67e620741dd0f4c71b2032a
bryce742 <bryce24franzen@gmail.com>
2024-02-04 06:30:44 -0600

    Initial commit

---
+++ s4h/home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views 
+
+urlpatterns =[
+    path("",views.index,name="index"), #run function when app is accessed, this is the index /hello page
+    path("learn",views.learn,name="learn"),
+    path("about",views.about,name="about"),
+    path("contributor",views.contribute_index,name="contribute_index"),
+    path("contributor/<str:contributor>",views.contribute,name="contribute"),
+    path("social_media",views.social_media,name="social_media"),
+    path("stories",views.stories,name="stories"),
+]
---
+++ s4h/home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "home/index.html")
+
+def learn(request):
+    return render(request,"home/learn.html")
+
+def about(request):
+    return render(request,"home/about.html")
+
+def contribute_index(request):
+    return render(request, "home/contributor_index.html")
+def contribute(request,contributor):
+    return render(request,"home/contribute.html", {
+        "contributor": contributor.capitalize()
+    })
+
+def social_media(request):
+    return render(request,"home/social_media.html")
+
+def stories(request):
+    return render(request,"home/stories.html")
